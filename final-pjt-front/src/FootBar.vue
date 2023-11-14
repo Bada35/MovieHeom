811c972794468bd506fe21a7a03a4318b6183a23
Bada35 <diduedidue@naver.com>
2023-11-15 02:12:04 +0900

    Feat: Skeleton File Structure

---
+++ final-pjt-front/src/FootBar.vue
@@ -0,0 +1,40 @@
+<template>
+      <footer>
+        <div style="background-color:rgb(230, 230, 230); font-size: 13px; ">
+          <div style="text-align:center;  margin-top:20px; color:grey">
+            <div style="padding-top: 5px; padding-bottom: 2px;">REMO</div>
+            <div style="padding-bottom: 2px;">SSAFY 10기</div>
+            <div class="name" style="width:12.5%; font-size: 15px;">Yeongjun Ryu | Jisu Seo</div>
+            <div style="padding-top: 2px; padding-bottom: 10px;">© 2023 All Rights Reserved.</div>
+          </div>
+        </div>
+      </footer>
+    </template>
+    
+    <script>
+    export default {
+      name: 'FootBar',
+      data() {
+        return {
+        }
+      }
+    
+    }
+    </script>
+    
+    <style scoped>
+    
+     .name{
+      background: linear-gradient(45deg, #9a9ae3, #ba68ed);
+      animation: hue-rotate 2s linear infinite alternate;
+      color: rgb(26, 26, 26);
+      border-radius: 5px;
+      display: inline-block;
+    
+    }
+    
+    @keyframes hue-rotate {
+      to{ filter: hue-rotate(90deg)}
+    }
+    
+    </style>
---
+++ final-pjt-front/src/views/Home/HomeView.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="home">
+      <h1>MovieHeeom</h1>
+      <div class="search">
+        <input type="text" placeholder="Where do you want to Heeom?" />
+        <button>Search</button>
+      </div>
+      <div class="navigation">
+        <button>Netflix</button>
+        <button>Watcha</button>
+        <button>BoxOffice</button>
+      </div>
+      <NetflixCard/>
+    </div>
+  </template>
+  
+  <script>
+  import NetflixCard from '@/components/HomeCards/NetflixCard.vue';
+  
+  export default {
+    name: 'HomeView',
+    components: {
+      NetflixCard
+    },
+    // 필요한 데이터, 메소드 등 추가
+  };
+  </script>
+  
+  <style scoped>
+  .home {
+    text-align: center;
+  }
+  
+  .search input {
+    margin-right: 10px;
+  }
+  
+  .navigation button {
+    margin-right: 10px;
+  }
+  
+  </style>
+  
