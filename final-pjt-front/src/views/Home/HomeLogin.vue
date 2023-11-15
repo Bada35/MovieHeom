7e64ffd9ab267d895ad2c561c76df99ad4ec84a4
Jisu Seo <diduedidue@naver.com>
2023-11-15 15:01:48 +0900

    Refactor: Home 컴포넌트 분리

---
+++ final-pjt-front/src/views/Home/HomeLogin.vue
@@ -0,0 +1,87 @@
+<template>
+    <div class="top-right-menu">
+        <button class="auth-button" @click="toggleLoginModal">로그인</button>
+        <button class="auth-button" @click="toggleSignUpModal">회원가입</button>
+    </div>
+    <div class="modal-overlay" v-if="showLoginModal" @click="toggleLoginModal">
+        <LoginModal @click.stop />
+    </div>
+    <div class="modal-overlay" v-if="showSignUpModal" @click="toggleSignUpModal">
+        <SignUpModal @click.stop />
+    </div>
+</template>
+  
+  
+<script>
+import { ref } from 'vue';
+import LoginModal from '@/components/Modal/LoginModal.vue';
+import SignUpModal from '@/components/Modal/SignUpModal.vue';
+
+export default {
+    name: 'HomeLogin',
+    components: {
+        LoginModal,
+        SignUpModal,
+    },
+    setup() {
+        const showLoginModal = ref(false);
+        const showSignUpModal = ref(false);
+
+        function toggleLoginModal() {
+            showLoginModal.value = !showLoginModal.value;
+        }
+
+        function toggleSignUpModal() {
+            showSignUpModal.value = !showSignUpModal.value;
+        }
+
+        return {
+            showLoginModal,
+            toggleLoginModal,
+            showSignUpModal,
+            toggleSignUpModal,
+        };
+    }
+};
+</script>
+  
+  
+<style scoped>
+.top-right-menu {
+    position: absolute;
+    top: 20px;
+    right: 20px;
+    display: flex;
+    align-items: center;
+}
+
+.auth-button {
+    padding: 5px 10px;
+    font-family: 'Nanum Gothic', sans-serif;
+    font-size: 0.8em;
+    background-color: #f0f0f0;
+    color: #333;
+    border: 1px solid #ddd;
+    border-radius: 4px;
+    cursor: pointer;
+    margin-left: 10px;
+}
+
+.auth-button:hover {
+    background-color: #e0e0e0;
+    border-color: #ccc;
+}
+
+.modal-overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: rgba(0, 0, 0, 0.5);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
+  
---
+++ final-pjt-front/src/views/Home/HomeNavigation.vue
@@ -0,0 +1,61 @@
+<template>
+      <div class="navigation">
+        <button @click="selectCard('Netflix')">Netflix</button>
+        <button @click="selectCard('Watcha')">Watcha</button>
+        <button @click="selectCard('BoxOffice')">BoxOffice</button>
+      </div>
+      <NetflixCard v-if="selectedCard === 'Netflix'" />
+      <WatchaCard v-if="selectedCard === 'Watcha'" />
+      <BoxOfficeCard v-if="selectedCard === 'BoxOffice'" />
+  </template>
+  
+  
+  <script>
+  import { ref } from 'vue';
+  import NetflixCard from '@/components/HomeCards/NetflixCard.vue';
+  import WatchaCard from '@/components/HomeCards/WatchaCard.vue';
+  import BoxOfficeCard from '@/components/HomeCards/BoxOfficeCard.vue';
+
+  
+  export default {
+    name: 'HomeNavigation',
+    components: {
+      NetflixCard,
+      WatchaCard,
+      BoxOfficeCard,
+    },
+    setup() {
+      const selectedCard = ref(null);
+  
+      function selectCard(card) {
+        selectedCard.value = card;
+      }
+  
+      return {
+        selectedCard,
+        selectCard,
+      };
+    }
+  };
+  </script>
+  
+  
+  <style scoped>
+  
+  .navigation button {
+    margin-right: 10px;
+    padding: 8px 16px;
+    font-family: 'Eczar', serif;
+    font-size: 1.1em;
+    background-color: #f0f0f0;
+    border: 2px solid #ddd;
+    border-radius: 5px;
+    cursor: pointer;
+  }
+  
+  .navigation button:hover {
+    background-color: #e0e0e0;
+  }
+  
+  </style>
+  
