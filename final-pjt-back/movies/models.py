dd50d476fd9dea950aeceb3783cf441e2f1ba3bb
hluuy <dudwns1289@gmail.com>
2023-11-17 15:19:00 +0900

    Create: Movie_DB & complete loaddata

--- final-pjt-back/movies/models.py
+++ final-pjt-back/movies/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Movie(models.Model):
+    movie_id = models.IntegerField(unique=True)
+    title = models.CharField(max_length=200)
+    released_date = models.DateField()
+    vote_avg = models.FloatField()
+    poster_path = models.CharField(max_length=200, blank=True, null=True)
+    genres = models.ManyToManyField(Genre)
+
+    def __str__(self):
+        return self.title
