5e6b638c3cbb518aff965cfea778efa3252ce7d0
hluuy <dudwns1289@gmail.com>
2023-11-17 17:08:40 +0900

    Update: movies app about searching at movie data
    django에 저장되어 있는 movie data에서 id 값을 통해 원하는 값을 검색
    "http://127.0.0.1:8000/movies/movies/?search=<movie_id>"의 형태로 사용

---
+++ final-pjt-back/movies/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import Movie, Genre
+
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = '__all__'
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = '__all__'
--- final-pjt-back/server/urls.py
+++ final-pjt-back/server/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('dj_rest_auth.urls')),
     path('accounts/signup/', include('dj_rest_auth.registration.urls')),
+    path('movies/', include('movies.urls')),
 ]
