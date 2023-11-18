1bf441aab274ab08111cb0a7e202f4f688df29b4
hluuy <dudwns1289@gmail.com>
2023-11-19 02:03:37 +0900

    Create: Profile feature
    프로필 구현
    GET 요청을 통해 프로필 페이지 접속 시, 'nickname', 'email', 'password', 'username', 'birth_date' 정보 나타남
    프로필 수정
    PUT 요청을 통해 한 번에 nickname과 email 그리고 password를 바꾸려고 했지만 PUT요청을 통해 password를 바꾸니 DB에 변경사항은 잘 업데이트 되었지만 변경된 password를 통해 로그인을 하려고 하니 로그인 오류 발생. 이후 django에서 제공하는 password change 기능 사용

--- final-pjt-back/accounts/urls.py
+++ final-pjt-back/accounts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import FollowUserView, FollowersListView, FollowingListView
+from .views import FollowUserView, FollowersListView, FollowingListView, UserProfileView, UserProfileEditView
 
 urlpatterns = [
     # 팔로우 관련 url
     path('follow/<str:username>/', FollowUserView.as_view(), name='follow-user'),
     path('users/<str:username>/followers/', FollowersListView.as_view(), name='followers-list'),
     path('users/<str:username>/followings/', FollowingListView.as_view(), name='followings-list'),
+    path('profile/', UserProfileView.as_view(), name='user-profile'),
+    path('profile/edit/', UserProfileEditView.as_view(), name='user-profile-edit'),
 ]
